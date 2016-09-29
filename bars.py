dd50ae6d0dfb6f38d4c0838dab92c5860ad2bf0d
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-09-30 01:37:14 +0300

    Исправлено вычисление расстояния

--- bars.py
+++ bars.py
@@ -2,69 +2,98 @@
 import json
 import sys
 import math
-
+import os
 reload(sys)
 import locale
 sys.setdefaultencoding(locale.getpreferredencoding())
 
 def load_data(filepath):
-    json_data = open(filepath)
-    data = json.load(json_data)
-    return data
-
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
 
 def get_biggest_bar(data):
-    big = data[0]
-    for num in data:
-        if num["Cells"]["SeatsCount"] > big["Cells"]["SeatsCount"]:
-            big = num
-    return big
+    biggest_bar = data[0]
+    for bar in data:
+        if bar["Cells"]["SeatsCount"] > biggest_bar["Cells"]["SeatsCount"]:
+            biggest_bar = bar
+
+    return biggest_bar
 
 
 def get_smallest_bar(data):
-    small = data[0]
-    for num in data:
-        if num["Cells"]["SeatsCount"] < small["Cells"]["SeatsCount"]:
-            small = num
-    return small
+    smallest_bar = data[0]
+    for bar in data:
+        if bar["Cells"]["SeatsCount"] < smallest_bar["Cells"]["SeatsCount"]:
+            smallest_bar = bar
+    return smallest_bar
 
-def ortodrom(latitude_1, longitude_1, latitude_2, longitude_2):  # Расчет ортодромии
+
+def get_ort_distance(latitude_1, longitude_1, latitude_2, longitude_2):  # Расчет ортодромии
     latitude_1 = math.radians(latitude_1)
     latitude_2 = math.radians(latitude_2)
     longitude_1 = math.radians(longitude_1)
     longitude_2 = math.radians(longitude_2)
-    res = math.acos(math.sin(latitude_1)*math.sin(latitude_2)
-                    +math.cos(latitude_1)*math.cos(latitude_2)*math.cos(longitude_2 - longitude_1))
-    return 111.7*res
+    earth_radius = 6371
+    latitude_delta = abs(latitude_2 - latitude_1)
+    longitude_delta = abs(longitude_2 - longitude_1)
+    central_angle = 2*math.asin(math.sqrt(math.pow(math.sin(latitude_delta/2), 2)
+                                          + math.cos(latitude_1)*math.cos(latitude_2)*math.pow(math.sin(longitude_delta/2), 2)))
+    return earth_radius*central_angle
+
 
 def get_closest_bar(data, longitude, latitude):
 
-    nearest = data[0]
-    near_ort = ortodrom(latitude, longitude, nearest["Cells"]["geoData"]["coordinates"][0],
-                        nearest["Cells"]["geoData"]["coordinates"][1])
-    for num in data:
-        new_ort = ortodrom(latitude, longitude, num["Cells"]["geoData"]["coordinates"][0],
-                           num["Cells"]["geoData"]["coordinates"][1])
-        if new_ort < near_ort:
-            near_ort = new_ort
-            nearest = num
-    return nearest
+    closest_bar = data[0]
+    least_distance = get_ort_distance(latitude, longitude, closest_bar["Cells"]["geoData"]["coordinates"][0],
+                        closest_bar["Cells"]["geoData"]["coordinates"][1])
+
+    for bar in data:
+        new_distance = get_ort_distance(latitude, longitude, bar["Cells"]["geoData"]["coordinates"][1],
+                                        bar["Cells"]["geoData"]["coordinates"][0])
+
+        if new_distance < least_distance:
+            least_distance = new_distance
+            closest_bar = bar
+    print(least_distance)
+    return closest_bar
 
 
 if __name__ == '__main__':
 
     # Ввод текущего местоположения
-    print(u'Введите координаты вашего местоположения')
-    a = float(input(u'Введите широту: '))
-    b = float(input(u'Введите долготу: '))
+    while True:
+        print(u'Введите координаты вашего местоположения')
+        try:
+            latitude = float(input(u'Введите широту: '))
+        except NameError:
+            latitude = None
+        if latitude is None:
+            print(u'Пожалуйста, введите широту заного')
+            continue
+        try:
+            longitude = float(input(u'Введите долготу: '))
+        except NameError:
+            if longitude is None:
+                print(u'Пожалуйста, введите местоположение заного')
+                continue
+        break
 
     data = load_data('Bars.json')
-    big = get_biggest_bar(data)
-    small = get_smallest_bar(data)
-    nearest = get_closest_bar(data, b, a)
-    # Вывод найденных баров
-    print(u'Самый большой бар: ' + big["Cells"]["Name"] + u', адрес: ' + big["Cells"]["Address"]
-          + u' количество мест: ' + str(big["Cells"]["SeatsCount"]))
-    print(u"Caмый маленький бар: " + small["Cells"]["Name"] + u', адрес: ' + small["Cells"]["Address"]
-          + u' количество мест: ' + str(small["Cells"]["SeatsCount"]))
-    print(u"Ближайший к вам бар: " + nearest["Cells"]["Name"] + u', адрес: ' + nearest["Cells"]["Address"])
+    if data is None:
+        print (u'Данных нет или указан неверный путь к файлу')
+    else:
+        biggest_bar = get_biggest_bar(data)
+        smallest_bar = get_smallest_bar(data)
+        closest_bar = get_closest_bar(data, longitude, latitude)
+        # Вывод найденных баров
+        print(u'Самый большой бар: %s, адрес: %s количество мест: %i' % (biggest_bar["Cells"]["Name"],
+                                                                         biggest_bar["Cells"]["Address"],
+                                                                         biggest_bar["Cells"]["SeatsCount"]))
+        print(u'Caмый маленький бар: %s, адрес: %s количество мест: %i' % (smallest_bar["Cells"]["Name"],
+                                                                           smallest_bar["Cells"]["Address"],
+                                                                           smallest_bar["Cells"]["SeatsCount"]))
+        print(u'Ближайший к вам бар: %s, адрес: %s количество мест: %i' % (closest_bar["Cells"]["Name"],
+                                                                           closest_bar["Cells"]["Address"],
+                                                                           closest_bar["Cells"]["SeatsCount"]))
